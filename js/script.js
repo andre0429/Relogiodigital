6cb67a26ccfcf52ad977c4657e92a845d0214ed9
André Lucas Barreto <andrelucas2904@gmail.com>
2025-08-17 20:15:42 -0300

    Commit final

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let mins = dateToday.getMinutes();         
+    let secs = dateToday.getSeconds();
+
+    if (hr < 10) hr  = '0' + hr;
+    if (mins < 10) mins = '0' + mins; 
+    if (secs < 10) secs = '0' + secs; 
+
+    horas.textContent = hr;
+    minutos.textContent = mins;
+    segundos.textContent = secs;
+
+})
